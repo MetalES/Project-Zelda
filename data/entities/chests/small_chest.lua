e10c497f38b7101fc3a36366d568fabe68496f47
MetalZelda <eternalstruggle.official@gmail.com>
2015-11-10 14:19:17 +0100

    shortened savegame_variable since the script allows prefixes

--- data/entities/chests/small_chest.lua
+++ data/entities/chests/small_chest.lua
@@ -4,7 +4,7 @@ local map = entity:get_map()
 local x_coordinate, y_coordinate = entity:get_position()
 local dungeon = game:get_dungeon_index()
 local mx, my = map:get_size()
-local chest_savegame_variable = "chest_" .. entity:get_name() .. "_" .. map:get_world() .. "_" .. mx .. "_" .. my .. "_" .. x_coordinate .. "_" .. y_coordinate
+local chest_savegame_variable = "chest_" .. entity:get_name() .. "_" .. map:get_world() .. "_" .. x_coordinate .. "_" .. y_coordinate
 local hero = entity:get_map():get_entity("hero")
 
 entity:add_collision_test("touching", function()
